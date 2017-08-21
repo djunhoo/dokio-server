d5d533a43976eb81d029f5fc8bf3ec48ef7f5cd7
djunhoo <cjdtjcjdtj@gmail.com>
2017-08-21 11:33:43 +0900

    제대로

--- config/auth.js
+++ config/auth.js
@@ -5,18 +5,18 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '133161083958817',
         'clientSecret'  : '472deedc4a30805ed210f5e797a741fc', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/users/auth/facebook/callback'
+        'callbackURL'   : 'http://keonho.xyz:3000/users/auth/facebook/callback'
     },
 
     'naverAuth' : {
         'clientID'       : 'BE0l52f9aEfCBb1pX_kH',
         'clientSecret'    : '1P2WHA7qjT',
-        'callbackURL'       : 'http://localhost:3000/users/auth/naver/callback'
+        'callbackURL'       : 'http://keonho.xyz:3000/users/auth/naver/callback'
     },
 
     'kakaoAuth' : {
         'clientID'      : 'cb93079bd2fbf2779ebef70596627ab5',
-        'callbackURL'   : 'http://localhost:3000/users/auth/kakao/callback'
-    }
-
+        'callbackURL'   : 'http://keonho.xyz:3000/users/auth/kakao/callback'
+    },
+    'jwt_secret' : 'keonho'
 };
